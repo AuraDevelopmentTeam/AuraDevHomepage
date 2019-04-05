5ca9911b81cf62c724acd1a3ec099b6d996f3470
Xeyame <xeyame@xeya.me>
2019-04-05 20:47:05 +0200

    Initial commit
    Very simple template of what the website should look like
    * No backend at all
    * Issues with CSS
      * Projects page cards not centering
      * Navbar doesn't go full width on larger screens
      * Navbar also needs to contract menu for mobile devices
    Probably going to ditch this theme and use the default bootstrap, but if someone sees hope in fixing this css mess thats great too.

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+  <head>
+   <title>Aura Development Team</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link href="./css/common.css" rel="stylesheet">
+    <!-- Page specific CSS -->
+    <link href="./css/index.css" rel="stylesheet">
+
+</head>
+<body>
+  <div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+      <div class="masthead clearfix mb-auto">
+        <div class="container inner">
+          <h3 class="masthead-brand">Aura Development Team</h3>
+          <nav class="masthead-nav">
+            <ul class="nav masthead-nav">
+              <li class="nav-link active"><a href="#">Home</a></li>
+              <li class="nav-link"><a href="./projects.php">Projects</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="inner cover">
+          <img class="homepage-image" src="https://f.xeya.me/f/2019/03/1554040868-FsxM0.svg">
+          <p class="lead">We are a small team of Minecraft Plugin Developers that works on their projects with passion and dedication.</p>
+          <p class="lead projects-button">
+            <a href="./projects.php" class="btn btn-lg btn-secondary">Our Projects</a>
+          </p>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+    <!-- jquery, Bootstrap & Popper JS-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ projects.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+  <head>
+   <title>Aura Development Team</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link href="./css/common.css" rel="stylesheet">
+    <!-- Page specific CSS -->
+    <link href="./css/projects.css" rel="stylesheet">
+
+</head>
+<body>
+  <div class="site-wrapper">
+    <div class="site-wrapper-inner">
+
+      <div class="masthead clearfix mb-auto">
+        <div class="container inner">
+          <h3 class="masthead-brand">Aura Development Team</h3>
+          <nav class="masthead-nav">
+            <ul class="nav masthead-nav">
+              <li class="nav-link"><a href="./index.php">Home</a></li>
+              <li class="nav-link active"><a href="#">Projects</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div class="card-deck text-center">
+        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+              <h5 class="card-title">Dark card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+            <div class="card-header">Header</div>
+            <div class="card-body">
+              <h5 class="card-title">Dark card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div>
+        <div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h5 class="card-title">Dark card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+    <!-- jquery, Bootstrap & Popper JS-->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </body>
+</html>
