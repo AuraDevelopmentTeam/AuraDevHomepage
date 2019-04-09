3ea8680886097f20a3f2ea34ee969fbb5972befc
Xeyame <xeyame@xeya.me>
2019-04-09 17:27:21 +0200

    Add settings file

---
+++ includes/settings.php
@@ -0,0 +1,8 @@
+<?php
+//Template for settings file, included into .gitignore
+
+//mysql settings
+$servername = "localhost";
+$username = "username";
+$password = "p4ssw0rd";
+$dbname = "database";
