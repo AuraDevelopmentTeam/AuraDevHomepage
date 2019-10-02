4fd40c97e84ce74e07f9941554b4c75b9613df0f
Xeyame <xeyame@xeya.me>
2019-10-02 13:12:44 +0200

    Some little fixes to make website viewable
    Still needs fixing with horizontal marging between cards, maybe downgrade to bootstrap 3 as 4 has horrible cards support. Also colors are wrong on chrome vs firefox somehow

--- includes/build_project_bootstrap_card.php
+++ includes/build_project_bootstrap_card.php
@@ -16,11 +16,14 @@
 function buildCards($projects_data) {
   //loop trough each "project" in $projects_data and insert values into template
   foreach ($projects_data as $value) {
-    echo "<div class=\"card text-white bg-dark mb-3\" style=\"max-width: 18rem;\">";
+    echo "<div class=\"card mb-3 mr-2 ml-3 text-white bg-dark mx-auto\" style=\"min-width:25rem; display: flex; margin-left: 35px; margin-right: 35px\">";
     //echo "    <div class=\"card-header\">" . $value["name"] . "</div>";
     echo "    <div class=\"card-body\">";
     echo "      <h5 class=\"card-title\">" . $value["name"] . "</h5>";
     echo "      <p class=\"card-text\">" . $value["description"] . "</p>";
+    echo "    </div>";
+    echo "    <div class=\"card-footer\">";
+    echo "      <small class=\"text-muted\"><a href=\"" . $value["url"] . "\">" . $value["url"] . "</a></small>";
     echo "    </div>";
     echo " </div>";
   }
