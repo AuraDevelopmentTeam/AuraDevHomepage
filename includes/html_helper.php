0c9530a03577823311276c15a8bcfaf1dea67633
BrainStone <yannick@tedworld.de>
2019-10-18 00:41:35 +0200

    Removed popper.js
    Not needed

--- includes/html_helper.php
+++ includes/html_helper.php
@@ -28,7 +28,6 @@ function html_head($title = 'Unknown Page Title', $page_specifc_css = false) {
 
     <!-- jquery, Bootstrap & Popper JS-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" defer></script>
 <?php
 }
