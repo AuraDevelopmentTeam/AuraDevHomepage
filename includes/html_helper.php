148dc97dacc84810989b160647d46bac6ce17050
BrainStone <yannick@tedworld.de>
2019-10-23 23:26:13 +0200

    Added missing integreties

--- includes/html_helper.php
+++ includes/html_helper.php
@@ -14,7 +14,7 @@ function html_head($title = 'Unknown Page Title', $page_specifc_css = false) {
    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Custom styles for this template -->
     <link href="/css/common.css" rel="stylesheet">
 <?php
@@ -27,8 +27,8 @@ function html_head($title = 'Unknown Page Title', $page_specifc_css = false) {
 ?>
 
     <!-- jquery & Bootstrap-->
-    <script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script defer src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <?php
 }
 
