c4094632738e54e4af014953a63b01171cb13cb3
BrainStone <yannick@tedworld.de>
2019-10-24 23:36:49 +0200

    Print shell output
    Else what's the point of interactive mode?

--- github_webhook.php
+++ github_webhook.php
@@ -3,5 +3,5 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  shell_exec('scripts/update_repo.sh -i');
+  echo shell_exec('scripts/update_repo.sh -i');
 }
