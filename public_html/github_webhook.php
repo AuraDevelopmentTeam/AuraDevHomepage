cdfb3209542c941900b7c6f5691817e562c790ab
BrainStone <yannick@tedworld.de>
2019-10-25 00:33:30 +0200

    Verify GitHub hook payload

--- public_html/github_webhook.php
+++ public_html/github_webhook.php
@@ -1,7 +1,32 @@
 <?php
 
-// Use in the "Post-Receive URLs" section of your GitHub repo.
+// Connect to mysql
+require_once '../includes/mysql_connection.php';
 
-if ( $_POST['payload'] ) {
+// Get page settings
+require_once '../includes/get_settings.php';
+$settings = loadSettings($conn);
+
+if ((isset($_POST['payload']) && !empty($_POST['payload'])) && (isset($_SERVER['HTTP_X_HUB_SIGNATURE']) && !empty($_SERVER['HTTP_X_HUB_SIGNATURE']))) {
+  // We got a payload. Let's verify!
+  $hash = 'sha1=' . hash_hmac('sha1', file_get_contents('php://input'), $settings['github_secret']);
+  if (strcmp($_SERVER['HTTP_X_HUB_SIGNATURE'], $hash) !== 0) {
+    // Verification failed. So client is unauthorized!
+    $error_code = 401;
+    require 'error.php';
+  }
+
+  // At this point we can trust the payload
+  $payload = json_decode($_POST['payload'], true);
+
+  if ($payload['ref'] !== ('refs/heads/' . $settings['branch']) {
+    die("Wrong branch\n");
+  }
+
+  // All ligths are green. Finally run script!
   echo shell_exec('../scripts/update_repo.sh -i 2>&1');
+} else {
+  // Let's pretend we don't exist
+  $error_code = 404;
+  require 'error.php';
 }
