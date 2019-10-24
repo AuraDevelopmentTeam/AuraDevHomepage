cb2236b5fa74f4eeb46ee9a3eb46542919e1a6f8
BrainStone <yannick@tedworld.de>
2019-10-24 19:35:13 +0200

    Properly indented the title and favicons

--- includes/html_helper.php
+++ includes/html_helper.php
@@ -8,10 +8,10 @@ function html_head($title = 'Unknown Page Title', $page_specifc_css = false) {
 <!doctype html>
 <html lang="en">
   <head>
-   <title><?php echo $title; ?></title>
+    <title><?php echo $title; ?></title>
 
-   <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
-   <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
