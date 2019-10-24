5642778fdad57be1a3a6f79dd58e33a50fdbe0b2
BrainStone <yannick@tedworld.de>
2019-10-24 23:47:32 +0200

    Show all output

--- github_webhook.php
+++ github_webhook.php
@@ -3,5 +3,5 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-  echo shell_exec('scripts/update_repo.sh -i');
+  echo shell_exec('scripts/update_repo.sh -i 2>&1');
 }
