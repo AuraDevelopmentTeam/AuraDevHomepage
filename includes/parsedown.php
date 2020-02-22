cd54b5a40d65a45d968f516df8bb8ec2190cfa17
BrainStone <yannick@tedworld.de>
2020-02-22 23:00:23 +0100

    Using new ParsedownExtended class

--- includes/parsedown.php
+++ includes/parsedown.php
@@ -5,19 +5,26 @@ require_once "parsedown_extra/ParsedownExtra.php";
 require_once "parsedown_extended/ParsedownExtended.php";
 
 function get_parsedown() {
-  $parsedown = new ParsedownExtended();
-
-  // Settings here
-  // See:
-  //  - https://github.com/erusev/parsedown#escaping-html
-  //  - https://github.com/erusev/parsedown-extra
-  //  - https://github.com/BenjaminHoegh/parsedown-extreme#new-features
-  $parsedown->toc([
-    'selector' => ['h1','h2','h3','h4','h5','h6'],
-    'inline' => true,
+  $parsedown = new ParsedownExtended([
+    // Settings here
+    // See:
+    //  - https://github.com/erusev/parsedown#escaping-html
+    //  - https://github.com/erusev/parsedown-extra
+    //  - https://github.com/BenjaminHoegh/ParsedownExtended#added-features
+    "toc" => [
+      "enable" => true,
+      "selector" => ["h1","h2","h3","h4","h5","h6"],
+      "inline" => true,
+    ],
+    "mark" => true,
+    "insert" => true,
+    "smartTypography" => true,
+    "scripts" => true,
+    "kbd" => true,
+    "task" => true,
+    "math" => true,
+    "diagrams" => true,
   ]);
-  $parsedown->katex(true);
-  $parsedown->mermaid(true);
 
   return $parsedown;
 }
